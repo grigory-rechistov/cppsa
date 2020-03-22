05f0bf16455f99ef7709050047ec1bdcf7cd66cf
Grigory Rechistov <grigory.rechistov@phystech.edu>
2020-03-23 00:52:18 +0100

    Add plans for more diagnostics

--- diagcodes.py
+++ diagcodes.py
@@ -8,5 +8,10 @@ diag_to_number = {
         "space_after_leading": 6,
         "unbalanced": 7,
         "unmarked_endif": 8,
+        # TODO "suggest_inline_function"
+        # TODO "undef_is_bad"
+        # TODO suggest_enum_or_const
+        # TODO remove_if_0_endif_dead_code
+        # TODO mixed_procent_and_sharp_directives: %ifdef #if %endif #endif
     }
 
